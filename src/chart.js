63f2b40b031c3f67d8226cba275e44be5e9aa9fa
Stéfan Sinclair <sgsinclair@users.noreply.github.com>
2018-09-27 10:01:47 -0400

    creating chart

---
+++ src/chart.js
@@ -0,0 +1,17 @@
+
+export function chart(target, data, config) {
+
+}
+
+class Chart {
+    this.target = target;
+    this.data = data;
+    constructor(target, data) {
+    }
+    bar(config) {
+    }
+    line(config) {
+    }
+    scatter(config) {
+    }
+}
