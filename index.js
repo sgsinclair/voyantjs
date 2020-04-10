b56855dc320c12c6a7b990afabee1aa39e36c3ef
Andrew James <ajmacdonald@users.noreply.github.com>
2020-04-10 13:35:14 -0400

    feat(categories): add CategoriesManager

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ import Corpus from "./src/corpus";
 import Table from "./src/table";
 import Load from "./src/load";
 import Chart from "./src/chart";
+import CategoriesManager from "./src/categories";
 
-export {Corpus, Table, Load, Chart};
+export {Corpus, Table, Load, Chart, CategoriesManager};
