c61bb33967a7c15d289ce9335f6e00ea32315b86
Andrew James <ajmacdonald@users.noreply.github.com>
2020-05-01 16:09:57 -0400

    fix(util): export util

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import Corpus from "./src/corpus";
 import Table from "./src/table";
 import Load from "./src/load";
+import Util from "./src/util";
 import Chart from "./src/chart";
 import Categories from "./src/categories";
 
-export {Corpus, Table, Load, Chart, Categories};
+export {Corpus, Table, Load, Util, Chart, Categories};
