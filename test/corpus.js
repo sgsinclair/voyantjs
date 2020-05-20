0ef242f0928bd5602cd0650e63982c3b07e3526c
Andrew James <ajmacdonald@users.noreply.github.com>
2020-05-20 13:18:18 -0400

    test: always add corpus metadata mock

--- test/corpus.js
+++ test/corpus.js
@@ -10,6 +10,7 @@ beforeAll(() => {
 
 beforeEach(() => {
 	fetch.resetMocks();
+	fetch.once(JSON.stringify(Mocks.CorpusMetadata))
 })
 
 test('id', () => {
@@ -41,146 +42,146 @@ test('summary', () => {
 	})
 })
 
-//test('titles', () => {
-//	fetch.once(JSON.stringify(Mocks.DocumentsMetadata));
-//	return Corpus.load(corpusId).then(corpus => {
-//		corpus.titles().then(data => {
-//			expect(data.length).toBe(8);
-//			expect(data[0]).toBe('121-0');
-//		});
-//	})
-//})
-//
-//test('text', () => {
-//	fetch.once(JSON.stringify(Mocks.CorpusTextsLimit100));
-//	return Corpus.load(corpusId).then(corpus => {
-//		corpus.text().then(data => {
-//			expect(data.length).toBe(849);
-//			expect(data.indexOf('Jane Austen')).toBe(58);
-//		});
-//	})
-//})
-//
-//test('texts', () => {
-//	fetch.once(JSON.stringify(Mocks.CorpusTextsLimit100));
-//	return Corpus.load(corpusId).then(corpus => {
-//		corpus.texts().then(data => {
-//			expect(data.length).toBe(8);
-//			expect(data[0].indexOf('Northanger Abbey')).toBe(37);
-//		});
-//	})
-//})
-//
-//test('terms', () => {
-//	fetch.once(JSON.stringify(Mocks.CorpusTermsLimit10));
-//	return Corpus.load(corpusId).then(corpus => {
-//		corpus.terms().then(data => {
-//			expect(data.length).toBe(10);
-//			expect(data[0].term).toBe('the');
-//			expect(data[0].rawFreq).toBe(29869);
-//		});
-//	})
-//})
-//
-//test('tokens', () => {
-//	fetch.once(JSON.stringify(Mocks.DocumentTokensLimit10));
-//	return Corpus.load(corpusId).then(corpus => {
-//		corpus.tokens().then(data => {
-//			expect(data.length).toBe(21);
-//			expect(data[4].term).toBe('Project');
-//			expect(data[4].rawFreq).toBe(87);
-//		});
-//	})
-//})
-//
-//test('words', () => {
-//	fetch.once(JSON.stringify(Mocks.DocumentTokensLimit10NoOthers));
-//	return Corpus.load(corpusId).then(corpus => {
-//		corpus.words().then(data => {
-//			expect(data.length).toBe(10);
-//			expect(data[6]).toBe('Abbey');
-//		});
-//	})
-//})
-//
-//test('contexts no query', () => {
-//	fetch.once(JSON.stringify(Mocks.DocumentContextsNoQuery));
-//	return Corpus.load(corpusId).then(corpus => {
-//		corpus.contexts().then(data => {
-//			expect(data.length).toBe(0);
-//		});
-//	})
-//})
-//
-//test('contexts love query', () => {
-//	fetch.once(JSON.stringify(Mocks.DocumentContextsLoveQuery));
-//	return Corpus.load(corpusId).then(corpus => {
-//		corpus.contexts({query: 'love'}).then(data => {
-//			expect(data.length).toBe(10);
-//			expect(data[0].term).toBe('love');
-//			expect(data[0].position).toBe(923);
-//		});
-//	})
-//})
-//
-//test('collocates no query', () => {
-//	fetch.once(JSON.stringify(Mocks.CorpusCollocatesNoQuery));
-//	return Corpus.load(corpusId).then(corpus => {
-//		corpus.collocates().then(data => {
-//			expect(data.length).toBe(0);
-//		});
-//	})
-//})
-//
-//test('collocates love query', () => {
-//	fetch.once(JSON.stringify(Mocks.CorpusCollocatesLoveQuery));
-//	return Corpus.load(corpusId).then(corpus => {
-//		corpus.collocates({query: 'love'}).then(data => {
-//			expect(data.length).toBe(10);
-//			expect(data[0].term).toBe('love');
-//			expect(data[0].contextTerm).toBe('and');
-//		});
-//	})
-//})
-//
-//test('phrases', () => {
-//	fetch.once(JSON.stringify(Mocks.CorpusNgramsLimit10));
-//	return Corpus.load(corpusId).then(corpus => {
-//		corpus.phrases().then(data => {
-//			expect(data.length).toBe(10);
-//			expect(data[0].term).toBe('of the');
-//			expect(data[0].length).toBe(2);
-//			expect(data[0].rawFreq).toBe(3430);
-//		});
-//	})
-//})
-//
-//test('correlations', () => {
-//	fetch.once(JSON.stringify(Mocks.CorpusTermCorrelationsLoveQuery));
-//	return Corpus.load(corpusId).then(corpus => {
-//		corpus.correlations({query: 'love'}).then(data => {
-//			expect(data.length).toBe(10);
-//			expect(data[0].source.term).toBe('the');
-//			expect(data[0].target.term).toBe('love');
-//			expect(data[0].correlation).toBe(0.22016151);
-//			expect(data[0].significance).toBe(0.6003425);
-//		});
-//	})
-//})
-//
-//test('ldaTopics', async () => {
-//	fetch
-//		.once(JSON.stringify(Mocks.Stopwords))
-//		.once(JSON.stringify(Mocks.CorpusTextsLimit500));
-//	const corpus = await Corpus.load(corpusId);
-//	const data = await corpus.ldaTopics({numberTopics: 5});
-//	expect(data.length).toBe(5);
-//})
-//
-//test('tool', () => {
-//	return Corpus.load(corpusId).then((c) => {
-//		return c.tool('scatterplot').then((url) => {
-//			expect(url).toContain('scatterplot');
-//		})
-//	})
-//})
+test('titles', () => {
+	fetch.once(JSON.stringify(Mocks.DocumentsMetadata));
+	return Corpus.load(corpusId).then(corpus => {
+		corpus.titles().then(data => {
+			expect(data.length).toBe(8);
+			expect(data[0]).toBe('121-0');
+		});
+	})
+})
+
+test('text', () => {
+	fetch.once(JSON.stringify(Mocks.CorpusTextsLimit100));
+	return Corpus.load(corpusId).then(corpus => {
+		corpus.text().then(data => {
+			expect(data.length).toBe(849);
+			expect(data.indexOf('Jane Austen')).toBe(58);
+		});
+	})
+})
+
+test('texts', () => {
+	fetch.once(JSON.stringify(Mocks.CorpusTextsLimit100));
+	return Corpus.load(corpusId).then(corpus => {
+		corpus.texts().then(data => {
+			expect(data.length).toBe(8);
+			expect(data[0].indexOf('Northanger Abbey')).toBe(37);
+		});
+	})
+})
+
+test('terms', () => {
+	fetch.once(JSON.stringify(Mocks.CorpusTermsLimit10));
+	return Corpus.load(corpusId).then(corpus => {
+		corpus.terms().then(data => {
+			expect(data.length).toBe(10);
+			expect(data[0].term).toBe('the');
+			expect(data[0].rawFreq).toBe(29869);
+		});
+	})
+})
+
+test('tokens', () => {
+	fetch.once(JSON.stringify(Mocks.DocumentTokensLimit10));
+	return Corpus.load(corpusId).then(corpus => {
+		corpus.tokens().then(data => {
+			expect(data.length).toBe(21);
+			expect(data[4].term).toBe('Project');
+			expect(data[4].rawFreq).toBe(87);
+		});
+	})
+})
+
+test('words', () => {
+	fetch.once(JSON.stringify(Mocks.DocumentTokensLimit10NoOthers));
+	return Corpus.load(corpusId).then(corpus => {
+		corpus.words().then(data => {
+			expect(data.length).toBe(10);
+			expect(data[6]).toBe('Abbey');
+		});
+	})
+})
+
+test('contexts no query', () => {
+	fetch.once(JSON.stringify(Mocks.DocumentContextsNoQuery));
+	return Corpus.load(corpusId).then(corpus => {
+		corpus.contexts().then(data => {
+			expect(data.length).toBe(0);
+		});
+	})
+})
+
+test('contexts love query', () => {
+	fetch.once(JSON.stringify(Mocks.DocumentContextsLoveQuery));
+	return Corpus.load(corpusId).then(corpus => {
+		corpus.contexts({query: 'love'}).then(data => {
+			expect(data.length).toBe(10);
+			expect(data[0].term).toBe('love');
+			expect(data[0].position).toBe(923);
+		});
+	})
+})
+
+test('collocates no query', () => {
+	fetch.once(JSON.stringify(Mocks.CorpusCollocatesNoQuery));
+	return Corpus.load(corpusId).then(corpus => {
+		corpus.collocates().then(data => {
+			expect(data.length).toBe(0);
+		});
+	})
+})
+
+test('collocates love query', () => {
+	fetch.once(JSON.stringify(Mocks.CorpusCollocatesLoveQuery));
+	return Corpus.load(corpusId).then(corpus => {
+		corpus.collocates({query: 'love'}).then(data => {
+			expect(data.length).toBe(10);
+			expect(data[0].term).toBe('love');
+			expect(data[0].contextTerm).toBe('and');
+		});
+	})
+})
+
+test('phrases', () => {
+	fetch.once(JSON.stringify(Mocks.CorpusNgramsLimit10));
+	return Corpus.load(corpusId).then(corpus => {
+		corpus.phrases().then(data => {
+			expect(data.length).toBe(10);
+			expect(data[0].term).toBe('of the');
+			expect(data[0].length).toBe(2);
+			expect(data[0].rawFreq).toBe(3430);
+		});
+	})
+})
+
+test('correlations', () => {
+	fetch.once(JSON.stringify(Mocks.CorpusTermCorrelationsLoveQuery));
+	return Corpus.load(corpusId).then(corpus => {
+		corpus.correlations({query: 'love'}).then(data => {
+			expect(data.length).toBe(10);
+			expect(data[0].source.term).toBe('the');
+			expect(data[0].target.term).toBe('love');
+			expect(data[0].correlation).toBe(0.22016151);
+			expect(data[0].significance).toBe(0.6003425);
+		});
+	})
+})
+
+test('ldaTopics', async () => {
+	fetch
+		.once(JSON.stringify(Mocks.Stopwords))
+		.once(JSON.stringify(Mocks.CorpusTextsLimit500));
+	const corpus = await Corpus.load(corpusId);
+	const data = await corpus.ldaTopics({numberTopics: 5});
+	expect(data.length).toBe(5);
+})
+
+test('tool', () => {
+	return Corpus.load(corpusId).then((c) => {
+		return c.tool('scatterplot').then((url) => {
+			expect(url).toContain('scatterplot');
+		})
+	})
+})
